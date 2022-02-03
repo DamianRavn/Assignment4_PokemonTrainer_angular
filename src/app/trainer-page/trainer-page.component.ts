18e2790fbe00e2c916517baa7ec0577af1059f1c
Igl0 <arneerne@gmail.com>
2022-02-03 18:05:02 +0100

    catalogue and pokemon api

--- src/app/trainer-page/trainer-page.component.ts
+++ src/app/trainer-page/trainer-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from "@angular/core";
 
+
 @Component({
     selector: 'app-trainer-page',
     templateUrl: './trainer-page.component.html',
     styleUrls: ['./trainer-page.component.html']
 })
 export class TrainerPageComponent{
+
 
 }
