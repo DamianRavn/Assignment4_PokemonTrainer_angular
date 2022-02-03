87c69e7f56647691745ec144c4b26a022900f3a2
Igl0 <arneerne@gmail.com>
2022-02-03 14:01:34 +0100

    added some pages, still needs individual views

---
+++ src/app/pokemon-catalogue-page/pokemon-catalogue-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-pokemon-catalogue-page',
+    templateUrl: './pokemon-catalogue-page.component.html',
+    styleUrls: ['./pokemon-catalogue-page.component.html']
+})
+export class PokemonCatalogueComponent{
+
+}
