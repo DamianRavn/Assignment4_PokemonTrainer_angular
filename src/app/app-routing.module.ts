ed5b513e7b4c6d2aa1b23a4fc513215bac760694
dragonalias <dragon1600@gmail.com>
2022-02-04 14:57:45 +0100

    Feat: basic Routing to websites implemented, even not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HttpClient } from  '@angular/common/http';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TrainerPageComponent } from './trainer-page/trainer-page.component';
+import { PokemonCatalogueComponent } from './pokemon-catalogue-page/pokemon-catalogue-page.component';
+import { NotFoundComponent } from './not-found-page/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  { 
+    path: '',
+    redirectTo: '/home', 
+    pathMatch: 'full'
+  },
+  { 
+    path: 'home',
+    component: LandingPageComponent 
+  },
+  { 
+    path: 'trainer',
+    component: TrainerPageComponent 
+  },
+  { 
+    path: 'pokemon',
+    component: PokemonCatalogueComponent 
+  },
+  {
+    path: '404',
+    component: NotFoundComponent
+  },
+  {
+    path: '**', 
+    redirectTo: '/404'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
