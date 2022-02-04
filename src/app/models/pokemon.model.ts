3d353ba70f9cc6d684e9b229fad50283d8f1ef2c
dragonalias <dragon1600@gmail.com>
2022-02-04 12:35:50 +0100

    feat: http request get username and loading variable

--- src/app/models/pokemon.model.ts
+++ src/app/models/pokemon.model.ts
@@ -2,3 +2,10 @@ export interface Pokemon{
     name: string;
     url: string; // use url extension to provide picture
 }
+
+export type User =
+{
+    id : number;
+    name : string;
+    pokemon : string[];
+}
--- src/app/requests/http-requests.ts
+++ src/app/requests/http-requests.ts
@@ -1,34 +1,50 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { User } from "../models/pokemon.model"
 
-type User =
-{
-    id : number;
-    name : string;
-    pokemon : string[];
-}
+const apiURL =  'https://experis-assignment-api.herokuapp.com'
+const apiKey =  'floppy-vitamin-cloud';
 
 @Injectable({providedIn: 'root'})
 export class PokemonService {
     constructor(private httpClient: HttpClient) { }
-    
-    apiURL : string = "https://assignment4-angular.herokuapp.com";
-    apiKey : string = 'your-public-api-key-goes-here';
 
-    public user : User | null = null;
+    private _loading : boolean = false;
+    get loading() : boolean
+    {
+        return this._loading;
+    }
+    //User data
+    defaultUser : User =
+    {
+        id: 0,
+        name: "default",
+        pokemon: []
+    }
+    private _trainer : User = this.defaultUser;
+    //Getter is read only
+    get trainer() : User
+    {
+        return this._trainer;
+    }
     getTrainer(username : string) : void
     {
-        this.httpClient.get<User>(`${this.apiURL}/trainers?username=${username}`)
+        this._loading = true;
+        this.httpClient.get<User>(`${apiURL}/trainers?username=${username}`)
         .subscribe
         ({
             next: (response : User)=>
             {
                 console.log("Response!",response);
-                this.user = response;
+                this._trainer = response;
             },
             error: (error : HttpErrorResponse)=>
             {
                 console.log(error);
+            },
+            complete: ()=>
+            {
+                this._loading = false;
             }
         });
     }
