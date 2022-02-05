62a94c7149af4298801dc8905bac3ff1f09c30af
dragonalias <dragon1600@gmail.com>
2022-02-05 21:08:08 +0100

    Fix: Small changes to html landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,3 @@
-import { style } from "@angular/animations";
 import { Component } from "@angular/core";
 
 @Component({
@@ -6,6 +5,6 @@ import { Component } from "@angular/core";
     templateUrl: './landing-page.component.html',
     styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent{
-
+export class LandingPageComponent
+{
 }
