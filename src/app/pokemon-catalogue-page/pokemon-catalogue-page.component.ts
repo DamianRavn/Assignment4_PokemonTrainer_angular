2164f2cf43d210607a0f2b49d04fc366171822a1
Igl0 <arneerne@gmail.com>
2022-02-07 16:39:41 +0100

    migrating pokemon get method from interface to service. WIP

--- src/app/pokemon-catalogue-page/pokemon-catalogue-page.component.ts
+++ src/app/pokemon-catalogue-page/pokemon-catalogue-page.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from "@angular/core";
 import { HttpClient } from  '@angular/common/http';
 import { Pokemon } from "../models/pokemon.model";
 
+let pictureVal = 1 // use string split on 6th '/' at  URL from the json file: https://pokeapi.co/api/v2/pokemon/1/"
+
 const URL = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=30/results"
+const pictureUrl =`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pictureVal}.png`
 
 @Component({
     selector: 'app-pokemon-catalogue-page',
@@ -11,28 +14,34 @@ const URL = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=30/results"
 })
 
 
-export class PokemonCatalogueComponent implements OnInit{
-
-    public pokemon: Pokemon | null = null;
-
-    constructor(private http: HttpClient){ 
-    }
+export class PokemonCatalogueComponent {
+    constructor() {}
     
-    
-    ngOnInit() {
-        this.http.get<Pokemon>( URL )    
-            .subscribe({
-                next: (response)=>{
-                    this.pokemon = response;
-                    //console.log("RESPONSE", response);
-                    //console.log(this.pokemon.results)
-                },
-                error: (error) => {
-                    console.log(error.message)
-                }
-            })
-    }
 }
+
+// export class PokemonCatalogueComponent implements OnInit{
+
+//     public pokemon: Pokemon | null = null;
+
+//     constructor(private http: HttpClient){ 
+//     }
+    
+    
+//     ngOnInit() {
+//         this.http.get<Pokemon>( URL )    
+//             .subscribe({
+//                 next: (response)=>{
+//                     this.pokemon = response;
+//                     //console.log("RESPONSE", response);
+//                     console.log(this.pokemon.results)
+//                 },
+//                 error: (error) => {
+//                     console.log(error.message)
+//                 }
+//             })
+//             console.log(pictureUrl)
+//     }
+// }
 
 // Notes for WIP
 // maybe the API fetch should be noved to its own folder as a component? (interface to th api)
